fe4678d2f6a9af32baa147bf8a54d7ba38604f49
TheLosser01 <victormvega03@gmail.com>
2022-05-06 19:49:37 -0500

    Taller04

---
+++ problema1/problema1/src/problema1/Ejecutable1.java
@@ -0,0 +1,20 @@
+
+package problema1;
+
+public class Ejecutable1 {
+    public static void main(String[] args) {
+        Promedio Prom1 = new Promedio("Enrique");
+        Promedio Prom2 = new Promedio ("Luis", 8.75);
+        
+        Prom1.establecerNota1(7.75);
+        Prom1.establecerNota2(8.0);
+        Prom1.establecernota3(8.05);
+        
+        
+        
+        
+        
+        
+    }
+    
+}
---
+++ problema1/problema1/src/problema1/Promedio.java
@@ -0,0 +1,68 @@
+
+package problema1;
+
+public class Promedio {
+    
+    public class Notas {
+        private String nombreEstudiante;
+        private double nota1;
+        private double nota2;
+        private double nota3;
+        private double promedio;
+        
+        public Promedio (String m){
+            nombreEstudiante = m;
+        }
+        
+        public Promedio (String nombre, double mat1){
+            nombreEstudiante = nombre;
+            nota1 = mat1;
+        }
+        
+        public void establecerNota1(double n){
+            nota1 = n;
+            
+        }
+        public void establecerNota2(double n){
+            nota2 = n;
+        }  
+        
+        public void establecerNota3(double n){
+            nota3 = n;
+        }
+        public void calcularPromedio(){
+            promedio = (nota1 + nota2 + nota3 / 3);
+        }
+        
+        
+        public double obtenerNota1(){
+            return nota1;
+        }
+        
+        public double obtenerNota2(){
+            return nota2;
+        }
+        
+        public double obtenerNota3(){
+            return nota3;
+        }
+        
+        public double obtenerPromedio(){
+            return promedio;
+        }
+        
+        @Override
+        public String toString() {
+        String cadena = String.format("Promedio estudiante\n"
+                + "Nombre estudiante: %s\n"
+                + "Materia 1: %.2f\n"
+                + "Materia 2: %.2f\n"
+                + "Materia 3: %.2f\n"
+                + "Promedio: %.2f\n",
+                nombreEstudiante, nota1, nota2, nota3, promedio);
+        return cadena;
+    }
+                
+    
+}
+}
